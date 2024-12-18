dc1d60a3e8f2413a1d1e91090ba190b249017339
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-18 16:14:02 +0700

    menyelesaikan HomeMkViewModel

--- app/src/main/java/com/example/ucproomdatabase_0162/ui/viewmodel/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_0162/ui/viewmodel/HomeMkViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.ucproomdatabase_0162.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucproomdatabase_0162.data.entity.MataKuliah
 import com.example.ucproomdatabase_0162.repository.RepositoryMk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMkViewModel (
     private val repositoryMk: RepositoryMk
@@ -14,12 +20,35 @@ class HomeMkViewModel (
     val homeuiState: StateFlow<HomeMkUiState> = repositoryMk.getAllMk()
         .filterNotNull()
         .map {
+            HomeMkUiState(
+                listMk = it.toList(),
+                isLoading = false,
+            )
 
         }
+        .onStart {
+            emit(HomeMkUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeMkUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeMkUiState(
+                isLoading = true,
+            )
+
+        )
+
 }
-
-
-
 
 data class HomeMkUiState(
     val listMk: List<MataKuliah> = listOf(),
