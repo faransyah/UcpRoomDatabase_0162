f63f0a3ba33ec39cd2b8e5dd27ea39b8fec047dd
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-18 13:29:29 +0700

    membuat KrsDatabase untuk dosen dan matakuliah

---
+++ app/src/main/java/com/example/ucproomdatabase_0162/data/database/KrsDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucproomdatabase_0162.data.dao.DosenDao
+import com.example.ucproomdatabase_0162.data.dao.MatakuliahDao
+import com.example.ucproomdatabase_0162.data.entity.Dosen
+import com.example.ucproomdatabase_0162.data.entity.MataKuliah
+
+@Database(entities = [Dosen::class, MataKuliah::class], version = 1, exportSchema = false)
+abstract class KrsDatabase : RoomDatabase(){
+
+    //mendefinisikan fungsi untuk mengakses data mahasiswa
+    abstract fun dosen(): DosenDao
+    abstract fun matakuliah(): MatakuliahDao
+
+    companion object{
+        @Volatile // Memastikan bahwa nilai variable Instance selalu sama di
+        private  var  Instance: KrsDatabase? = null
+
+        fun getDatabase(context: Context): KrsDatabase {
+            return (Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    KrsDatabase::class.java, // Class database
+                    "KrsDatabase" // Nama Database
+                )
+                    .build().also{ Instance = it }
+            })
+        }
+    }
+}
