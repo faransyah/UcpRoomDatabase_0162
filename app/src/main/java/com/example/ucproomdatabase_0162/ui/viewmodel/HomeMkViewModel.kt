d64aa6e43434625fb4c4303bca6b3c99dd954fd8
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-18 16:08:48 +0700

    membuat variabel

--- app/src/main/java/com/example/ucproomdatabase_0162/ui/viewmodel/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_0162/ui/viewmodel/HomeMkViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.ucproomdatabase_0162.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.ucproomdatabase_0162.data.entity.MataKuliah
+import com.example.ucproomdatabase_0162.repository.RepositoryMk
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+
+class HomeMkViewModel (
+    private val repositoryMk: RepositoryMk
+): ViewModel(){
+
+    val homeuiState: StateFlow<HomeMkUiState> = repositoryMk.getAllMk()
+        .filterNotNull()
+        .map {
+
+        }
+}
+
+
+
 
 data class HomeMkUiState(
     val listMk: List<MataKuliah> = listOf(),
