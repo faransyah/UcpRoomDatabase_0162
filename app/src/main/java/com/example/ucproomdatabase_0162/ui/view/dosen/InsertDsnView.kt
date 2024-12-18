4492888602068dcee6125a81e74207383e0c2891
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-12-18 20:07:33 +0700

    membuat variabel didalam InsertDsnView

--- app/src/main/java/com/example/ucproomdatabase_0162/ui/view/dosen/InsertDsnView.kt
+++ app/src/main/java/com/example/ucproomdatabase_0162/ui/view/dosen/InsertDsnView.kt
@@ -1,6 +1,9 @@
 package com.example.ucproomdatabase_0162.ui.view.dosen
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucproomdatabase_0162.ui.navigation.AlamatNavigasi
@@ -18,5 +21,7 @@ fun InsertDsnView(
     modifier: Modifier = Modifier,
     viewModel: DosenViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    val uiState = viewModel.uiState
+    val SnackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 }
